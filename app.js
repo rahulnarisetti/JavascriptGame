f18861d04318f625133385aae44f1f087971c71f
Rahul Narisetti <rahul.narisetti@gmail.com>
2019-06-08 04:54:42 +0530

    second commit

--- app.js
+++ app.js
@@ -9,16 +9,31 @@ GAME RULES:
 
 */
 
-
-var scores=[0,0],roundScore=0,activePlayer=1,dice;
-dice=Math.floor((Math.random()*6)+1);
-
-
-document.querySelector('#current-'+activePlayer).textContent=dice;
+// document.querySelector('#current-'+activePlayer).textContent=dice;
 
 
 
-var x=document.querySelector('#score-'+activePlayer).textContent;
-console.log(x);
-//document.querySelector('#current-0').innerHTML='<b>'+x+'</b>'
+// var x=document.querySelector('#score-'+activePlayer).textContent;
+// console.log(x);
+// //document.querySelector('#current-0').innerHTML='<b>'+x+'</b>'
+
+
+var scores=[0,0],roundScore=0,activePlayer=1;
+
+
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
+
 document.querySelector('.dice').style.display='none';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+    var dice=Math.floor((Math.random()*6)+1);
+
+    var diceDOM=document.querySelector('.dice');
+    diceDOM.style.display='block';  
+    diceDOM.src='dice-'+dice+'.png';
+
+});
